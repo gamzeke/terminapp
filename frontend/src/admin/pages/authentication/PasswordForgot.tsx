4123a3f283559bba017809c806b0797cee2c3f2a
Gamze Ketenci <gmz.ktnc@gmail.com>
2022-11-10 16:20:29 +0100

    Cleans up the password reset page

--- frontend/src/admin/pages/authentication/PasswordForgot.tsx
+++ frontend/src/admin/pages/authentication/PasswordForgot.tsx
@@ -1,54 +1,21 @@
-import { Anchor, Box, Center, createStyles, Text, Title } from '@mantine/core';
+import { Anchor, Box, Center, Text, Title } from '@mantine/core';
 import { IconArrowLeft } from '@tabler/icons';
-import { useState } from 'react';
 import { AuthStatusType } from './AuthView';
 
 interface PasswordForgotProps {
     updateAuthStatus: (state: AuthStatusType) => void;
 }
 
-const useStyles = createStyles(theme => ({
-    title: {
-        fontSize: 26,
-        fontWeight: 900,
-        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    },
-
-    controls: {
-        [theme.fn.smallerThan('xs')]: {
-            flexDirection: 'column-reverse',
-        },
-    },
-
-    control: {
-        [theme.fn.smallerThan('xs')]: {
-            width: '100%',
-            textAlign: 'center',
-        },
-    },
-}));
-
 const PasswordForgot = ({ updateAuthStatus }: PasswordForgotProps) => {
-    const { classes } = useStyles();
-
-    const [mail, setMail] = useState('');
-
-    const resetPasswordHandler = () => {
-        //TODO-MMUEJDE: Ein Passwort Reset Link anfordern
-    };
-
     return (
         <>
-            <Title className={classes.title} align="center">
-                Passwort vergessen
-            </Title>
+            <Title align="center">Passwort vergessen</Title>
             <Text color="dimmed" size="sm" align="center">
                 Bitte wenden Sie sich Ihrem Administrator
             </Text>
             <Anchor
                 color="dimmed"
                 size="sm"
-                className={classes.control}
                 onClick={() => {
                     updateAuthStatus('LOGIN');
                 }}
@@ -58,40 +25,6 @@ const PasswordForgot = ({ updateAuthStatus }: PasswordForgotProps) => {
                     <Box ml={5}>Zurück</Box>
                 </Center>
             </Anchor>
-            {/*<Paper withBorder shadow="md" p={30} radius="md" mt="xl">
-                 <TextInput
-                    value={mail}
-                    onChange={event => {
-                        setMail(event.currentTarget.value);
-                    }}
-                    label="Deine E-Mail"
-                    placeholder="max.mustermann@muster.de"
-                    required
-                />
-                <Group mt="lg" className={classes.controls}>
-                    <Anchor
-                        color="dimmed"
-                        size="sm"
-                        className={classes.control}
-                        onClick={() => {
-                            updateAuthStatus('LOGIN');
-                        }}
-                    >
-                        <Center>
-                            <IconArrowLeft size={12} stroke={1.5} />
-                            <Box ml={5}>Zurück</Box>
-                        </Center>
-                    </Anchor>
-                    <Button
-                        onClick={() => {
-                            resetPasswordHandler();
-                        }}
-                        className={classes.control}
-                    >
-                        Passwort zurücksetzen
-                    </Button>
-                </Group>
-            </Paper>/*/}
         </>
     );
 };
